2bb448d5b0a6a1448ee2971d3e589495031e1a1f
Tomaso <66296094+IsaakThaddaeus@users.noreply.github.com>
2024-01-09 00:12:17 +0100

    Add files via upload

--- PointInPolygon.cs
+++ PointInPolygon.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class PointInPolygon
 {
-    public static bool inside(Vector2 a, List<Vector2> polygon, float tolerance)
+    public static bool inside(Vector2 a, List<Vector2> polygon)
     {
         int intersections = 0;
         Vector2 b = a + new Vector2(10000, 0);
@@ -14,7 +14,7 @@ public static class PointInPolygon
             Vector2 c = polygon[i];
             Vector2 d = polygon[(i + 1) % polygon.Count];
 
-            if(Vector2.Distance(c, a) + Vector2.Distance(d, a) <= Vector2.Distance(c, d) + tolerance)
+            if(Vector2.Distance(c, a) + Vector2.Distance(d, a) == Vector2.Distance(c, d))
             {
                 return true;
             }
@@ -25,11 +25,31 @@ public static class PointInPolygon
             }
         }
 
-        if(intersections % 2 == 0)
+        return false;
+    }
+
+
+    public static bool inside(Vector2 a, List<Vector2> polygon, float tolerance)
+    {
+        int intersections = 0;
+        Vector2 b = a + new Vector2(10000, 0);
+
+        for (int i = 0; i < polygon.Count; i++)
         {
-            return false;
+            Vector2 c = polygon[i];
+            Vector2 d = polygon[(i + 1) % polygon.Count];
+
+            if (Vector2.Distance(c, a) + Vector2.Distance(d, a) <= Vector2.Distance(c, d) + tolerance)
+            {
+                return true;
+            }
+
+            if (LineIntersector.doIntersect(a, b, c, d))
+            {
+                intersections++;
+            }
         }
 
-        return true;
+        return false;
     }
 }
