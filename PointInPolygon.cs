bd5dc5f78d27abaa6ef7c502a2437061f326b2f9
Tomaso <66296094+IsaakThaddaeus@users.noreply.github.com>
2024-01-09 21:47:28 +0100

    Add files via upload

--- PointInPolygon.cs
+++ PointInPolygon.cs
@@ -1,38 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public static class PointInPolygon
 {
-    public static bool inside(Vector2 a, List<Vector2> polygon)
+
+    public static bool insideAngle(Vector2 a, List<Vector2> polygon, float tolerance)
     {
-        int intersections = 0;
-        Vector2 b = a + new Vector2(10000, 0);
+        double sum = 0;
 
-        for(int i = 0; i < polygon.Count; i++)
+        for (int i = 0; i < polygon.Count; i++)
         {
-            Vector2 c = polygon[i];
-            Vector2 d = polygon[(i + 1) % polygon.Count];
+            Vector2 b = polygon[i];
+            Vector2 c = polygon[(i + 1) % polygon.Count];
 
-            if(Vector2.Distance(c, a) + Vector2.Distance(d, a) == Vector2.Distance(c, d))
+            if (Vector2.Distance(b, a) + Vector2.Distance(c, a) <= Vector2.Distance(b, c) + tolerance)
             {
                 return true;
             }
 
-            if(LineIntersector.doIntersect(a, b, c, d))
+
+            sum += Vector2.SignedAngle(b - a, c - a);
+        }
+
+        sum = Mathf.Abs((float)sum);
+
+        if (sum > 359)
+            return true;
+
+        return false;
+    }
+    public static bool insideRay(Vector2 a, List<Vector2> polygon)
+    {
+        int intersections = 0;
+        Vector2 b = a + new Vector2(10000, 0);
+
+        for (int i = 0; i < polygon.Count; i++)
+        {
+            Vector2 c = polygon[i];
+            Vector2 d = polygon[(i + 1) % polygon.Count];
+
+            if (Vector2.Distance(c, a) + Vector2.Distance(d, a) == Vector2.Distance(c, d))
+            {
+                return true;
+            }
+
+            if (LineIntersector.doIntersect(a, b, c, d))
             {
                 intersections++;
             }
         }
 
-        if((intersections % 2) == 0)
+        if ((intersections % 2) == 0)
         {
             return false;
         }
 
         return true;
     }
-    public static bool inside(Vector2 a, List<Vector2> polygon, float tolerance)
+    public static bool insideRay(Vector2 a, List<Vector2> polygon, float tolerance)
     {
         int intersections = 0;
         Vector2 b = a + new Vector2(10000, 0);
@@ -60,5 +87,4 @@ public static class PointInPolygon
 
         return true;
     }
-
 }
